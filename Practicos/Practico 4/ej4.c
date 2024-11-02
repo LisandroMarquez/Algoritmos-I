ad6e87ebb1a4b24b8b2600e1a9a380bfabd18058
Lichi <lichimarquez04@gmail.com>
2024-11-02 17:25:15 -0300

    Ejercicio 4 corregido

--- Practicos/Practico 4/ej4.c
+++ Practicos/Practico 4/ej4.c
@@ -1,9 +1,11 @@
 //+ Imports
 #include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 
 //+ Functions
 int minimo(int x, int y) {
+    assert(x != y);
     if (x <= y){
         return x;
     } else {
@@ -13,12 +15,7 @@ int minimo(int x, int y) {
 }
 
 int valorAbs(int x) {
-    if (x >= 0) {
-        return x;
-    } else {
-        return (-x);
-    }
-    return 0;
+    return (x >= 0) ? x : -x;
 }
 
 //+ Main
@@ -42,16 +39,26 @@ int main(void)
         printf("Ingrese y: ");
         scanf("%i", &y);
 
+        //* Corroborar con assert
+        assert(x != y);
+
         //! Logic
-        printf("min(%i, %i) = %i\n", x, y, minimo(x,y));
+        int min = minimo(x, y);
+        printf("min(%i, %i) = %i\n", x, y, min);
+        assert(min == x || min == y);
         break;
     case 2:
         //% Required inputs
         printf("\nIngrese x: ");
         scanf("%i", &x);
 
+        //* Corroborar con assert
+        assert(x != y);
+
         //! Logic
-        printf("|%i| = %i\n", x, valorAbs(x));
+        int absVal = valorAbs(x);
+        printf("|%i| = %i\n", x, absVal);
+        assert(absVal >= 0);
         break;
     default:
         //! Logic
